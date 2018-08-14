36789f2afef3a41e531f2a56adc4f53a0e4d0a05
Piyush Mehta <me@piyushmehta.com>
2018-08-15 00:08:29 +0530

    done

---
+++ notes.js
@@ -0,0 +1,23 @@
+const Notes = [{
+  Department: "ISE",
+  Subject: "DBMS",
+  Link: "https://linkwillbehere.com/yesHereOnly",
+  id: 1
+}, {
+  Department: "CSE",
+  Subject: "DBMS",
+  Link: "https://linkwillbehere.com/yesHereOnly",
+  id: 2
+}, {
+  Department: "CV",
+  Subject: "MAT-3",
+  Link: "https://linkwillbehere.com/yesHereOnly",
+  id: 3
+}, {
+  Department: "ISE",
+  Subject: "DMS",
+  Link: "https://linkwillbehere.com/yesHereOnly",
+  id: 4
+}, ];
+
+module.exports.Notes = Notes;
