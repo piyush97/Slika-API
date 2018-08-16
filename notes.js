766dbc6ea916ad447c13f1931d8de98312549a00
Piyush Mehta <me@piyushmehta.com>
2018-08-16 22:54:08 +0530

    check

--- notes.js
+++ notes.js
@@ -1,12 +1,14 @@
 const Notes = [{
-  Department: "ISE",
-  Subject: "DBMS",
-  Link: "https://linkwillbehere.com/yesHereOnly",
+  Department: "CV",
+  Subject: "Design of Pre Stressed Concrete",
+  Sem: "7",
+  Link: "https://github.com/MyCollegeForums/Backups/raw/master/uploads/default/original/1X/0c95d1a8f463d85ab390a78fc051d8e3e855f6d4.PDF",
   id: 1
 }, {
-  Department: "CSE",
-  Subject: "DBMS",
-  Link: "https://linkwillbehere.com/yesHereOnly",
+  Department: "CV",
+  Subject: "Estimation and Costing",
+  Sem: "7",
+  Link: "https://github.com/MyCollegeForums/Backups/raw/master/uploads/default/original/1X/0fd81ce560375b657ba90849b96a29a33485087d.PDF",
   id: 2
 }, {
   Department: "CV",
