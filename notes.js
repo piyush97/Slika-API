26cb9f1525758bc217819106ed508877cd7b9584
Piyush Mehta <me@piyushmehta.com>
2018-08-16 22:59:54 +0530

    check

--- notes.js
+++ notes.js
@@ -2,24 +2,14 @@ const Notes = [{
   Department: "CV",
   Subject: "Design of Pre Stressed Concrete",
   Sem: "7",
-  Link: "https://github.com/MyCollegeForums/Backups/raw/master/uploads/default/original/1X/0c95d1a8f463d85ab390a78fc051d8e3e855f6d4.PDF",
+  Link: "https://bit.ly/2MQe38w",
   id: 1
 }, {
   Department: "CV",
   Subject: "Estimation and Costing",
   Sem: "7",
-  Link: "https://github.com/MyCollegeForums/Backups/raw/master/uploads/default/original/1X/0fd81ce560375b657ba90849b96a29a33485087d.PDF",
+    Link: "https://bit.ly/2nFtSUC",
   id: 2
-}, {
-  Department: "CV",
-  Subject: "MAT-3",
-  Link: "https://linkwillbehere.com/yesHereOnly",
-  id: 3
-}, {
-  Department: "ISE",
-  Subject: "DMS",
-  Link: "https://linkwillbehere.com/yesHereOnly",
-  id: 4
-}, ];
+}];
 
 module.exports.Notes = Notes;
