6cd8286605f7f372c2efcf1ccee1ea985ecddd7a
Piyush Mehta <me@piyushmehta.com>
2018-08-19 15:36:46 +0530

    crud
    Signed-off-by: Piyush Mehta <me@piyushmehta.com>

--- models/notes.js
+++ models/notes.js
@@ -1,8 +1,10 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
 
-var NoteSchema = new Schema({
-  name: String
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+let NoteSchema = new Schema({
+  description: String
 });
 
 module.exports = mongoose.model('notes', NoteSchema);
