148e513bfbf576324ad6d83dad109358d4be3615
Piyush Mehta <me@piyushmehta.com>
2018-08-19 15:09:21 +0530

    crud
    Signed-off-by: Piyush Mehta <me@piyushmehta.com>

--- server.js
+++ server.js
@@ -19,23 +19,11 @@ const mustacheExpressInstance = mustacheExpress();
 mustacheExpressInstance.cache = null;
 app.engine('mustache', mustacheExpressInstance);
 app.set('view engine', 'mustache');
-app.set('views', __dirname + '/views');
+app.set('views', `${__dirname}/views`);
 
-app.post('/notes', (req, res) => {
-  const schema = {
-    Department: Joi.string().min(1).required(),
-  };
-  const result = Joi.validate(req.body, schema);
-  console.log(result);
-
-  if (result.error) {
-    res.status(400).send("Error");
-  }
-});
+app.get('/', (req, res) => res.render('index', {}));
 
 app.get('/notes', (req, res) => (res.send(Notes)));
 
 const PORT = 8080;
 app.listen(process.env.PORT || PORT);
-
-console.log(`Running on http://localhost:${PORT}`);
