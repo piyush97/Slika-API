b25205a07686f3122d15ffc3076e3a83ec0db04c
Piyush Mehta <me@piyushmehta.com>
2018-08-19 14:21:44 +0530

    crud
    Signed-off-by: Piyush Mehta <me@piyushmehta.com>

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
 
 const express = require('express');
 
+const mustacheExpress = require('mustache-express');
+
+const bodyParser = require('body-parser');
+
+const mongoose = require('mongoose');
+
 const app = express();
+
+const Joi = require('joi');
 
 const Notes = require('./notes');
 
+app.post('/notes', (req, res) => {
+  const schema = {
+    Department: Joi.string().min(1).required(),
+  };
+  const result = Joi.validate(req.body, schema);
+  console.log(result);
+
+  if (result.error) {
+    res.status(400).send("Error");
+  }
+});
+
 app.get('/notes', (req, res) => (res.send(Notes)));
 
-const PORT = 3000;
+const PORT = 8080;
 app.listen(process.env.PORT || PORT);
 
 console.log(`Running on http://localhost:${PORT}`);
