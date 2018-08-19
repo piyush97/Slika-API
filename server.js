5562e68880f1c0f871fc2f3f5e500fc00cf7a7f7
Piyush Mehta <me@piyushmehta.com>
2018-08-19 15:36:58 +0530

    crud
    Signed-off-by: Piyush Mehta <me@piyushmehta.com>

--- server.js
+++ server.js
@@ -1,17 +1,10 @@
 
 const express = require('express');
-
 const mustacheExpress = require('mustache-express');
-
 const bodyParser = require('body-parser');
-
-// const mongoose = require('mongoose');
+const Notes = require('./notes');
 
 const app = express();
-
-// const Joi = require('joi');
-
-const Notes = require('./notes');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
