290de7d789e562f4644b204d70db3513087a51b3
Piyush Mehta <me@piyushmehta.com>
2018-08-19 13:27:31 +0530

    Typo solved
    Signed-off-by: Piyush Mehta <me@piyushmehta.com>

--- server.js
+++ server.js
@@ -7,4 +7,7 @@ const Notes = require('./notes');
 
 app.get('/notes', (req, res) => (res.send(Notes)));
 
-app.listen(process.env.PORT || 8080);
+const PORT = 3000;
+app.listen(process.env.PORT || PORT);
+
+console.log(`Running on http://localhost:${PORT}`);
