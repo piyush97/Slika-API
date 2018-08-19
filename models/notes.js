dc6341d539781b5f6536d7bcdf796fe7ae9ac8ff
Piyush Mehta <me@piyushmehta.com>
2018-08-19 19:05:38 +0530

    Mongodb working

--- models/notes.js
+++ models/notes.js
@@ -7,7 +7,8 @@ const NoteSchema = new Schema({
   Department: String,
   Subject: String,
   Sem: Number,
-  
+  Link: String,
+  id: Number,
 });
 
 module.exports = mongoose.model('notes', NoteSchema);
