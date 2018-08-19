cf6c1b70944b5ab78fb327fe7d5982536593f751
Piyush Mehta <me@piyushmehta.com>
2018-08-19 18:25:30 +0530

    Mongodb working

--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,12 @@
 // Routes
 const router = require('express').Router();
-const Notes =  require('../models/notes');
+const Notes = require('../models/notes');
 // const notes = require('./notes');
 
 router.get('/', (req, res) => res.render('index', {}));
 
 router.post('/', (req, res) => {
-  let newNote = new Notes({ description: req.body.description });
+  const newNote = new Notes({ description: req.body.description });
 
   newNote
     .save()
