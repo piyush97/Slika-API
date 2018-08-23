5220e869c85981e439bb7aa55ca054f361cbed56
Piyush Mehta <me@piyushmehta.com>
2018-08-23 14:53:13 +0530

    validations
    Signed-off-by: Piyush Mehta <me@piyushmehta.com>

--- validation/register.js
+++ validation/register.js
@@ -4,8 +4,13 @@ const isEmpty = require('./is-empty');
 
 module.exports = function validateRegisterInput(data) {
   let errors = {};
+  data.name = !isEmpty(data) ? data.name : '';
+
   if (!Validator.isLength(data.name, { min: 6, max: 30 })) {
     errors.name = 'Name must be between 6 and 30 characters';
+  }
+  if (Validator.isEmpty(data.name)) {
+    errors.name = 'Name field is required';
   }
   return {
     errors,
