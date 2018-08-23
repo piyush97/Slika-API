65b5de0fde5ef8239864062f4476d9904d4cbc48
Piyush Mehta <me@piyushmehta.com>
2018-08-23 11:25:15 +0530

    ESLint config
    Signed-off-by: Piyush Mehta <me@piyushmehta.com>

--- routes/routes.js
+++ routes/routes.js
@@ -78,8 +78,28 @@ router.post('/register', (req, res) => {
 // @Route Post
 
 router.post('/login', (req, res) => {
-  const email = req.body.email,
-  const password = req.body.password,
-})
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by Email
+  User.findOne({ email })
+    .then((user) => {
+      // Check for User
+      if (!user) {
+        return res.status(404).json({ email: 'User not found' });
+      }
+
+      // Check Password
+
+      bcrypt.compare(password, user.password)
+        .then((isMatch) => {
+          if (isMatch) {
+            res.json({ msg: 'Success' });
+          } else {
+            return res.status(400).json({ password: 'Password Incorrect'});
+          }
+        });
+    });
+});
 
 module.exports = router;
