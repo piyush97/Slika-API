3931f405b84e8fc93e4775d5ae1b0b337a8c8f56
Piyush Mehta <me@piyushmehta.com>
2018-08-23 13:14:02 +0530

    current protected route
    Signed-off-by: Piyush Mehta <me@piyushmehta.com>

--- routes/routes.js
+++ routes/routes.js
@@ -125,6 +125,10 @@ router.post('/login', (req, res) => {
 // @Route GET
 // @Access Private
 router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
-  res.json({ msg: 'Success' });
+  res.json({
+    id: req.user.id,
+    name: req.user.name,
+    email: req.user.email,
+  });
 });
 module.exports = router;
