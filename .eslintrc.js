6615053dcb30ecf9dedb72181a509ca7a81cfc02
Piyush Mehta <me@piyushmehta.com>
2018-08-23 11:25:01 +0530

    ESLint config
    Signed-off-by: Piyush Mehta <me@piyushmehta.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,12 @@
 module.exports = {
-      rules: {
-          'no-console': 0,
-      },
+    rules: {
+        'no-console': 0,
+        "prefer-destructuring": ["error", {
+            "array": false,
+            "object": false
+        }, {
+            "enforceForRenamedProperties": false
+        }]
+    },
     "extends": "airbnb-base"
 };
