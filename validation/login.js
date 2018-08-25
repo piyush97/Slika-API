bbb24dd32904f3b96a8f41e98b40a1949d27e6b3
Piyush Mehta <me@piyushmehta.com>
2018-08-25 18:03:36 +0530

    validation for login done

--- validation/login.js
+++ validation/login.js
@@ -4,10 +4,8 @@ const isEmpty = require('./is-empty');
 
 module.exports = function validateLoginInput(data) {
   let errors = {};
-  data.name = !isEmpty(data.name) ? data.name : '';
   data.email = !isEmpty(data.email) ? data.email : '';
   data.password = !isEmpty(data.password) ? data.password : '';
-  data.password2 = !isEmpty(data.password) ? data.password2 : '';
 
   if (Validator.isEmpty(data.email)) {
     errors.email = 'email field is required';
@@ -15,9 +13,7 @@ module.exports = function validateLoginInput(data) {
   if (Validator.isEmpty(data.password)) {
     errors.password = 'password is Invalid';
   }
-  if (Validator.isLength(data.password, { min: 6, max: 30 })) {
-    errors.password = 'Password must be atleast 6 Characters';
-  }
+  
   if (Validator.isEmpty(data.password)) {
     errors.password = 'Password is Required';
   }
