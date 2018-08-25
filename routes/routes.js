98e6c26f0ce25f2a22b37584b37afb38eddc1944
Piyush Mehta <me@piyushmehta.com>
2018-08-25 18:50:42 +0530

    profile error handling
    Signed-off-by: Piyush Mehta <me@piyushmehta.com>

--- routes/routes.js
+++ routes/routes.js
@@ -158,9 +158,17 @@ router.get('/current', passport.authenticate('jwt', { session: false }), (req, r
 router.get('/', passport.authenticate('jwt', {
   session: false,
 }), (req, res) => {
+  const errors = {};
+
   Profile.findOne({
-    
-  });
+    user: req.user.id,
+  })
+    .then((profile) => {
+      if (!profile) {
+        errors.noprofile = 'There is no profile for this user';
+        return res.status(404).json(errors);
+      }
+    });
 });
 
 module.exports = router;
