471d879b24390382483feda808cda714cbcb04f2
Piyush Mehta <me@piyushmehta.com>
2018-08-26 10:13:58 +0530

    profile validation

---
+++ validation/profile.js
@@ -0,0 +1,24 @@
+const Validator = require('validator');
+
+const isEmpty = require('./is-empty');
+
+module.exports = function validateProfileInput(data) {
+  let errors = {};
+  data.handle = !isEmpty(data.handle) ? data.handle : '';
+  data.college = !isEmpty(data.college) ? data.college : '';
+
+  if (!Validator.isEmpty(data.handle)) {
+    errors.handle = 'handle field is required';
+  }
+  if (Validator.isEmpty(data.status)) {
+    errors.status = 'status is Invalid';
+  }
+
+  if (Validator.isEmpty(data.status)) {
+    errors.status = 'status is Required';
+  }
+  return {
+    errors,
+    isValid: isEmpty(errors),
+  };
+};
