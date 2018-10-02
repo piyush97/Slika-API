c3252972ac78954258de7923e5201c416571453e
Mathías <mathiasd88@gmail.com>
2018-10-02 20:15:53 -0300

    Add: Test suite and structure. Issue #20

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,11 @@
-module.exports = {
+const configuration = {
   mongoURI: 'mmongodb://piyush:piyush1@ds123852.mlab.com:23852/slika',
   secretOrKey: 'secret',
 };
+
+if (process.env.NODE_ENV === 'test') {
+  configuration.mongoURI = 'mmongodb://:@localhost:27017/slika';
+  configuration.secretOrKey = 'secret';
+}
+
+module.exports = configuration;
